5c0b19d6ecd83277ab637505280dd51406ebadc0
Dania Sankar <dania.j.sankar@gmail.com>
2023-04-16 02:09:16 +0400

    implement jest and react test libraries

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/New Construction Service Request/i);
   expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole("heading")).toHaveTextContent(/New Construction Service/i);
+  expect(screen.getByRole("img")).toBeInTheDocument();
+ // expect(screen.queryByRole("heading")).toHaveTextContent(/payment is done/i);
 });
